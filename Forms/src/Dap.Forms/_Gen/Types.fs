08812ee5b3832f772148e2cedc9554aa7cadd1a0
YJ Park <yjpark@gmail.com>
2018-09-19 13:18:46 +0800

    follow lib changes

--- Forms/src/Dap.Forms/_Gen/Types.fs
+++ Forms/src/Dap.Forms/_Gen/Types.fs
@@ -12,8 +12,10 @@ open Dap.Local
 type IFormsPackArgs =
     inherit ILocalPackArgs
     abstract Temp : FileSystemArgs with get
+    abstract AsLocalPackArgs : ILocalPackArgs with get
 
 type IFormsPack =
     inherit IPack
     inherit ILocalPack
     abstract Args : IFormsPackArgs with get
+    abstract AsLocalPack : ILocalPack with get
