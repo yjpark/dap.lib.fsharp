f7e871511f00d0bc28baf4913277501914846860
YJ Park <yjpark@gmail.com>
2018-11-08 16:50:16 +0800

    follow core change with Runtime

--- Local/src/Dap.Local/Helper.fs
+++ Local/src/Dap.Local/Helper.fs
@@ -5,25 +5,11 @@ open Dap.Prelude
 open Dap.Platform
 
 type FileSinkArgs with
-    static member LocalCreate (root : string, filename : string, ?minLevel : LogLevel, ?rolling : RollingInterval) =
+    member this.WithTimestamp () =
         let timestamp = getNow' () |> instantToText
         let timestamp = timestamp.Replace (":", "_")
-        let path = System.IO.Path.Combine (root, timestamp, filename)
-        FileSinkArgs.Create (path, ?minLevel = minLevel, ?rolling = rolling)
-
-type LoggingArgs with
-    static member LocalCreate (?consoleLogLevel : LogLevel, ?filename : string, ?fileLogLevel : LogLevel, ?rolling : RollingInterval) =
-        let consoleSink =
-            consoleLogLevel
-            |> Option.map (fun consoleLogLevel ->
-                ConsoleSinkArgs.Create (minLevel = consoleLogLevel)
-            )
-        let fileSink =
-            filename
-            |> Option.map (fun filename ->
-                FileSinkArgs.LocalCreate ("log", filename, ?minLevel = fileLogLevel, ?rolling = rolling)
-            )
-        LoggingArgs.Create (?console = consoleSink, ?file = fileSink)
+        System.IO.Path.Combine (this.Folder, timestamp, this.Filename)
+        |> this.WithPath
 
 type IAppPack with
     member this.PreferencesProps = this.Preferences.Context.Properties
