b97127873edce2ec085dcc70aff69a63f40672b4
YJ Park <yjpark@gmail.com>
2018-11-25 18:55:59 +0800

    add WebHost.setStaticRoot

--- Remote/src/Dap.Remote.Web/WebHost.fs
+++ Remote/src/Dap.Remote.Web/WebHost.fs
@@ -2,6 +2,8 @@
 module Dap.Remote.Web.WebHost
 
 open System
+open System.IO
+
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Diagnostics
@@ -12,11 +14,17 @@ open Dap.Prelude
 open Dap.Platform
 
 type WebHost = {
+    Root : string option
     Urls : string []
     Actions : (IApplicationBuilder -> unit) list
     ServicesActions : (IServiceCollection -> unit) list
 } with
     member this.Build (builder : IWebHostBuilder) =
+        this.Root
+        |> Option.iter (fun root ->
+            builder.UseContentRoot (root) |> ignore
+            builder.UseWebRoot (root) |> ignore
+        )
         builder.UseUrls this.Urls |> ignore
         builder.Configure (Action<IApplicationBuilder> (fun host ->
             this.Actions
@@ -29,6 +37,7 @@ type WebHost = {
         builder.Build ()
     static member empty =
         {
+            Root = None
             Urls = [| |]
             Actions = []
             ServicesActions = []
@@ -63,6 +72,16 @@ type WebHost with
         WebHost.setupServices (action >> ignore)
 
 type WebHost with
+    static member setStaticRoot (root : string) =
+        let root = Path.Combine (Directory.GetCurrentDirectory(), root);
+        fun (this : WebHost) ->
+            {this with Root = Some root}
+            |> WebHost.setupBatch [
+                fun h -> h.UseDefaultFiles ()
+                fun h -> h.UseStaticFiles ()
+            ]
+
+type WebHost with
     static member setDevMode =
         WebHost.setupBatch [
             fun h -> h.UseDeveloperExceptionPage ()
@@ -73,12 +92,12 @@ type WebHost with
 
 type WebHost with
     static member setWebSocketHub (env : IEnv, path : string, kind : Kind) =
-        WebHost.setup (fun h -> h.UseWebSockets ())
-        >> WebHost.setup (WebSocketHub.useWebSocketHub env path kind)
+        WebHost.setup (fun h ->
+            h.UseWebSockets ()
+        )>> WebHost.setup (WebSocketHub.useWebSocketHub env path kind)
     static member setWebSocketHubs (env : IEnv, hubs : (string * Kind) list) =
         WebHost.setup (fun h -> h.UseWebSockets ())
         >> WebHost.setupBatch [
             for (path, kind) in hubs do
                 yield WebSocketHub.useWebSocketHub env path kind
         ]
-
