f2038354cea0500cffa12e7c9935846977679597
YJ Park <yjpark@gmail.com>
2019-08-03 03:57:57 +0000

    add W.Space

--- src/Fable.Dap.Fulma/Types.fs
+++ src/Fable.Dap.Fulma/Types.fs
@@ -1,6 +1,29 @@
 [<AutoOpen>]
 module Dap.Fulma.Types
 
-open Dap.React
+//SILP: FULMA_VIEW_IMPORTS()
+open Fable.Core                                                       //__SILP__
+open Fulma                                                            //__SILP__
+open Fable.FontAwesome                                                //__SILP__
+module H = Fable.React.Helpers                                        //__SILP__
+module R = Fable.React.Standard                                       //__SILP__
+module P = Fable.React.Props                                          //__SILP__
+                                                                      //__SILP__
+open Dap.Prelude                                                      //__SILP__
+open Dap.Context                                                      //__SILP__
+open Dap.Platform                                                     //__SILP__
+open Dap.React                                                        //__SILP_
 
 type W = DapFulmaWidgets
+with
+    static member Space
+        (
+            ?px : int
+        ) =
+        let px = defaultArg px 10
+        R.span [
+            P.Style [
+                P.CSSProp.MarginRight (sprintf "%ipx" px)
+            ]
+        ] [H.str "&nbsp;"]
+
